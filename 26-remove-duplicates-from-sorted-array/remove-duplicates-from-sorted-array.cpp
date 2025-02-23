a0e34ea748d4052f94a23eca2275e9559614b93a
skyhammer1995 <39307121+skyhammer1995@users.noreply.github.com>
2025-02-23 00:57:48 -0500

    Time: 0 ms (100.00%) | Memory: 22.7 MB (19.83%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        // base case
-        if (nums.size() == 1) {
-            return 1;
+        // Second attempt, reduce
+        int k = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] != nums[k-1]) {
+                nums[k] = nums[i];
+                ++k;
+            }
         }
 
-        std::vector<int>::iterator unique = nums.begin();
-        std::vector<int>::iterator current = nums.begin() + 1;
-
-        while (current != nums.end()) {
-            if (*current != *unique) {
-                // new unique element, move iterator/pointer to where current points
-                unique = current; 
-                ++current;
-            } else if (*current == *unique) {
-               // duplicate element, remove duplicate
-                current = nums.erase(current);
-            }
-        }        
-        return nums.size();
+        return k;
     }
 };
