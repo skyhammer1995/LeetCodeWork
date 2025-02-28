874d12e9f5940b4459b5ca0a4611702f469dacb5
skyhammer1995 <39307121+skyhammer1995@users.noreply.github.com>
2025-02-27 21:29:36 -0500

    Time: 0 ms (100.00%) | Memory: 17 MB (83.23%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int totalProfit = 0;
+        int currentMaxProfit = 0;
+        int minBuy = prices[0];
+
+        for (const auto& price : prices) {
+            if (price < minBuy) {
+                minBuy = price;
+            } else if ((price - minBuy) > currentMaxProfit) {
+                currentMaxProfit = price - minBuy;
+                // sell immediately, since we can just buy today, and "step up" profit
+                totalProfit += currentMaxProfit;
+                currentMaxProfit = 0;
+                minBuy = price;
+            } 
+        }
+        return std::max(totalProfit, currentMaxProfit);
+    }
+};
