753d1c2b3670fef16680dc487969ccc3007ce29d
skyhammer1995 <39307121+skyhammer1995@users.noreply.github.com>
2025-08-16 14:11:12 -0400

    Time: 1 ms (4.74%) | Memory: 8 MB (70.53%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.c
@@ -0,0 +1,31 @@
+int strStr(char* haystack, char* needle) {
+    int n = strlen(haystack);
+    int m = strlen(needle);
+
+    if (m == 0) {
+        // empty string for needle
+        return 0;
+    }
+    if (m > n) {
+        // needle is longer than the haystack
+        return -1;
+    }
+
+    for (int i = 0; i <= n - m; i++) {
+        int j = 0;
+        while (j < m && haystack[i + j] == needle[j]) {
+            // laymans: while the value of J is less than the max length of string `needle`,
+            // AND the i'th (fixed technically when we're in here) + j'th element is equal to the j'th element of needle
+            // we will continue and iterate on j. Once j == m, or we don't match, we will break out 
+            // and check if we're a match. If not, we'll just iterate to the next i'th element.
+            j++;
+        }
+
+        if (j == m) {
+            // full match
+            return i;
+        }
+    }
+
+    return -1;
+}
