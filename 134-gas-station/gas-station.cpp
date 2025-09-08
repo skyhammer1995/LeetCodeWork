2bdf866480e16fecb794c0065a8491f0b98974d0
skyhammer1995 <39307121+skyhammer1995@users.noreply.github.com>
2025-09-07 23:08:40 -0400

    Time: 3 ms (35.71%) | Memory: 112.5 MB (43.74%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -5,16 +5,6 @@ public:
         int currentGas = 0; // current tank while simulating a section
         int solution = 0; 
 
-        // Start at where gas[i] is max? If we ever get to a point where cost[i] > currentGas, we have to return -1?
-        // could it be even simpler to say, the sum of all elements in vector gas, and cost, we know we can go around the circuit?
-
-        // Intuition: Ok, stepping through the first example, I think we can just start at 0, keep a running sum:
-        // current gas + gas[i] - current[i];
-        // once that sum equates to 0 (or greater), we can choose i as our starting index (I think). We may need to get to the end of the arrays
-        // otherwise, if we never hit 0 or greater, we can return -1
-
-        // Ok, more greedy research, we need to just consider that 'if we ever hit where the tank < 0, any stations between last solution, and current i would not have worked.
-        
         for (int i = 0; i < gas.size(); ++i) {
             int difference = gas[i] - cost[i];
             total += difference;
